29e28f1b137528b1f73ae67a60f704a0b6a9b45d
Danny Sanchez <dsanchez@tronc.com>
2018-11-21 13:29:44 -0500

    Write out flattened CSV files named the same as the XML file.

--- pbso_jail_records.py
+++ pbso_jail_records.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 from termcolor import colored
 from bs4 import BeautifulSoup
 
@@ -23,65 +24,79 @@ booking_data = soup.find_all("Booking")
 
 item_counter = 0
 
-"""
-The tag structure here is <Booking><Cases><Charges><BIO>
+csv_fieldnames = ["FullName", "birthdate", "Race", "address1", "city", "state", "zipcode", "BookingDate", "BookingTime", "BookingId", "Jacket", "InmateFirst", "InmateMiddle", "InmateLast", "InmateSuffix", "ReleaseDate", "ReleaseTime", "Gender", "ChargeCode", "ChargeDescription", "currentbond", "bondamount", "OBTS", "facility", "cell", "holdotheragencies"]
 
-The Booking tag contains biographical details. Inmates can have multiple cases and multiple charges within a case. It's a tree of one-to-manys. The BIOs all seem to be dupes, so this script just gets the first one.
-"""
-for booking in booking_data:
+with open(filename.replace(".xml", "") + ".csv", 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=csv_fieldnames)
+    writer.writeheader()
 
-    booking_attributes = ["FullName", "birthdate", "Race", "address1", "city", "state", "zipcode", "BookingDate", "BookingTime", "BookingId", "Jacket", "InmateFirst", "InmateMiddle", "InmateLast", "InmateSuffix", "ReleaseDate", "ReleaseTime", "Gender"]
+    """
+    The tag structure in the jail file is <Booking><Cases><Charges><BIO>
 
-    charge_attributes = ["ChargeCode", "ChargeDescription", "currentbond", "bondamount"]
+    The Booking tag contains biographical details. Inmates can have multiple cases and multiple charges within a case. It's a tree of one-to-manys. The BIOs all seem to be dupes, so this script just gets the first one.
+    """
+    for booking in booking_data:
 
-    bio_attributes = ["OBTS", "facility", "cell", "holdotheragencies"]
+        booking_attributes = ["FullName", "birthdate", "Race", "address1", "city", "state", "zipcode", "BookingDate", "BookingTime", "BookingId", "Jacket", "InmateFirst", "InmateMiddle", "InmateLast", "InmateSuffix", "ReleaseDate", "ReleaseTime", "Gender"]
 
-    # Set an empty string to catch missing attributes in the data file.
-    for attribute in booking_attributes:
-        if booking.has_attr(attribute) is False:
-            booking[attribute] = ""
+        charge_attributes = ["ChargeCode", "ChargeDescription", "currentbond", "bondamount"]
 
-    booking_data = {
-        "FullName": booking["FullName"],
-        "birthdate": booking["birthdate"],
-        "Race": booking["Race"],
-        "address1": booking["address1"],
-        "city": booking["city"],
-        "state": booking["state"],
-        "zipcode": booking["zipcode"],
-        "BookingDate": booking["BookingDate"],
-        "BookingTime": booking["BookingTime"],
-        "BookingId": booking["BookingId"],
-        "Jacket": booking["Jacket"],
-        "InmateFirst": booking["InmateFirst"],
-        "InmateMiddle": booking["InmateMiddle"],
-        "InmateLast": booking["InmateLast"],
-        "InmateSuffix": booking["InmateSuffix"],
-        "ReleaseDate": booking["ReleaseDate"],
-        "ReleaseTime": booking["ReleaseTime"],
-        "Gender": booking["Gender"]
-    }
+        bio_attributes = ["OBTS", "facility", "cell", "holdotheragencies"]
 
-    # Drill down into the list of cases.
-    cases = booking.find_all("Cases")
-    for case in cases:
-        if case.has_attr("ArrestingAgency") is False:
-            case["ArrestingAgency"] = ""
+        # Set an empty string to catch missing attributes in the data file.
+        for attribute in booking_attributes:
+            if booking.has_attr(attribute) is False:
+                booking[attribute] = ""
 
-        # Drill down into the charges for the case.
-        charges = case.find_all("Charges")
-        for charge in charges:
-            for attribute in charge_attributes:
-                if charge.has_attr(attribute) is False:
-                    charge[attribute] = ""
+        # Drill down into the list of cases.
+        cases = booking.find_all("Cases")
+        for case in cases:
+            if case.has_attr("ArrestingAgency") is False:
+                case["ArrestingAgency"] = ""
 
-            # Drill down into the bio for the case.
-            bio = charge.find("BIO")  # Just get the first one
+            # Drill down into the charges for the case.
+            charges = case.find_all("Charges")
+            for charge in charges:
+                for attribute in charge_attributes:
+                    if charge.has_attr(attribute) is False:
+                        charge[attribute] = ""
 
-            for attribute in bio_attributes:
-                if bio.has_attr(attribute) is False:
-                    bio[attribute] = ""
+                # Drill down into the bio for the case.
+                bio = charge.find("BIO")  # Just get the first one
 
-    item_counter += 1
+                for attribute in bio_attributes:
+                    if bio.has_attr(attribute) is False:
+                        bio[attribute] = ""
+
+                writer.writerow({
+                    "FullName": booking['FullName'],
+                    "birthdate": booking['birthdate'],
+                    "Race": booking['Race'],
+                    "address1": booking['address1'],
+                    "city": booking['city'],
+                    "state": booking['state'],
+                    "zipcode": booking['zipcode'],
+                    "BookingDate": booking['BookingDate'],
+                    "BookingTime": booking['BookingTime'],
+                    "BookingId": booking['BookingId'],
+                    "Jacket": booking['Jacket'],
+                    "InmateFirst": booking['InmateFirst'],
+                    "InmateMiddle": booking['InmateMiddle'],
+                    "InmateLast": booking['InmateLast'],
+                    "InmateSuffix": booking['InmateSuffix'],
+                    "ReleaseDate": booking['ReleaseDate'],
+                    "ReleaseTime": booking['ReleaseTime'],
+                    "Gender": booking['Gender'],
+                    "ChargeCode": charge['ChargeCode'],
+                    "ChargeDescription": charge['ChargeDescription'],
+                    "currentbond": charge['currentbond'],
+                    "bondamount": charge['bondamount'],
+                    "OBTS": bio['OBTS'],
+                    "facility": bio['facility'],
+                    "cell": bio['cell'],
+                    "holdotheragencies": bio['holdotheragencies']
+                })
+
+        item_counter += 1
 
 print("Number of bookings: " + "{:,}".format(item_counter))
